18d481ffeee0f6a3c3e12093a3293ae9cb176834
EugeneShevoldaev <161762648+EugeneShevoldaev@users.noreply.github.com>
2024-12-08 21:51:22 +0300

    Add files via upload

---
+++ шифр_3.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randint(3, 20)  # получаем случайное число в заданном диапазоне
+password = []  # создаем пустой список
+
+for i in range(1, 20):  # перебор всех значений для подбора шифра
+    for j in range(1, 20):
+        if number % (i + j) == 0:
+            pas = (i, j)
+            revers_pas = (j, i)
+            if pas not in password and revers_pas not in password:
+                password.append(pas)
+
+
+
+print('cod: ', number)  # вывод на печать полученного рандомно значения кода
+print('пароль: ', *password)  # вывод на печать полученного пароля
